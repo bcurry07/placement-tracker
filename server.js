0a0df5ee31a005f6da448d7a019ef7eea8eda5d7
Bob Curry <bcurry07@gmail.com>
2014-08-05 15:41:28 -0400

    commented out favicon

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var favicon = require('static-favicon');
+//var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -23,7 +23,7 @@ var app = express();
 app.set('views', path.join(__dirname, '/server/views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
