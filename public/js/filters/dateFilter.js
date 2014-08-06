865416f1feaf94ea17da5a0f791f239cbe4da87b
Bob Curry <bcurry07@gmail.com>
2014-08-06 15:11:10 -0400

    date filter

--- public/js/filters/dateFilter.js
+++ public/js/filters/dateFilter.js
@@ -3,7 +3,7 @@ angular.module('app').filter('dateFilter', function($filter) {
 
        var ngFilteredDate = $filter('date')(item, 'd');
        var trueDate = new Date(item).getDate();
-        console.log("ngFilteredDate is " + ngFilteredDate + " and trueDate is " + trueDate);
+        console.log("ngFilteredDate is " + ngFilteredDate + " and trueDate is " + trueDate + " and item was " + item);
        if (ngFilteredDate == trueDate) return $filter('date')(item, 'shortDate');
        else {
            var hour = item.getHours();
