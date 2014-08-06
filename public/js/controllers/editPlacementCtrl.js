fdc2a8f25e638d81ab4b3d0930c789f780954daa
Bob Curry <bcurry07@gmail.com>
2014-08-06 15:41:07 -0400

    date filter

--- public/js/controllers/editPlacementCtrl.js
+++ public/js/controllers/editPlacementCtrl.js
@@ -9,7 +9,7 @@ angular.module('app').controller('editPlacementCtrl', function($scope, $location
 
 
             var placement = editingPlacement.selectedPlacement;
-           placement.date = $filter('date')(placement.date, 'shortDate');
+           placement.date = $filter('dateFilter')(placement.date);
             $scope.placement = placement;
 
 
