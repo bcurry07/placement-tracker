4f1e9d6b22c424b2a624c1d42303b25b5bcd3856
Bob Curry <bcurry07@gmail.com>
2015-01-23 09:03:40 -0500

    testing commit from webstorm

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@ angular.module('app', ['ngResource','ngRoute']);
 angular.module('app').config(function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
-        //this routes the user to the various html pages
+        //this routes the user to the various html pagesd
         //when the url takes the user to the 'when' location then it diverts them to the template url
         //which uses the controller shown
         //this ultimately pings the server-side routing which further directs the user along their way
