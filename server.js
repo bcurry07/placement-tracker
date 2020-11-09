e806d499b1d6f516dd27f7cf2b8d40cc120fb92a
Robert Curry <bcurry07@gmail.com>
2020-11-09 18:50:01 -0500

    refactor

--- server.js
+++ server.js
@@ -52,13 +52,15 @@ if(env === 'development') {
   mongoose.connect('mongodb://localhost/untitled');
 } else {
   var newString = 'mongodb+srv://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PW + '@placements.tekqa.mongodb.net/placements?retryWrites=true&w=majority';
-  var oldString = 'mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PW + 'placements-shard-00-00.tekqa.mongodb.net:27017,placements-shard-00-01.tekqa.mongodb.net:27017,placements-shard-00-02.tekqa.mongodb.net:27017/placements?ssl=true&replicaSet=atlas-7w6rq8-shard-0&authSource=admin&retryWrites=true&w=majority';
+  var oldString = 'mongodb://' + process.env.MONGO_USERNAME + ':' + process.env.MONGO_PW + '@placements-shard-00-00.tekqa.mongodb.net:27017,placements-shard-00-01.tekqa.mongodb.net:27017,placements-shard-00-02.tekqa.mongodb.net:27017/placements?ssl=true&replicaSet=atlas-7w6rq8-shard-0&authSource=admin&retryWrites=true&w=majority';
   var promise = mongoose.connect(oldString, {
     useMongoClient: true,
   });
 
+  console.log(process.env.MONGO_USERNAME);
+  console.log( process.env.MONGO_PW);
   promise.then(function(db) {
-    console.log('db opened!');
+    console.log('callback from the db');
   });
 }
 
